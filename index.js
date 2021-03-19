272b9e71d4a047dcb533678357b3ee922160c38d
kiranFullStack <kiran.fullstack@outlook.com>
2021-03-19 23:43:18 +0530

    .env created

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
+require('dotenv/config')
 
-const PORT = 3000
+const PORT = process.env.PORT
 
 app.get('/', (req, res) => {
     res.send('HI FROM THE SERVER!')
