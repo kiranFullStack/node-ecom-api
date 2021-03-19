6d28d9f430b3f31af3b35f3bbc554f52d944d482
kiranFullStack <kiran.fullstack@outlook.com>
2021-03-19 23:37:46 +0530

    Basic Server created

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+const PORT = 3000
+
+app.get('/', (req, res) => {
+    res.send('HI FROM THE SERVER')
+})
+
+app.listen(PORT, () => {
+    console.log(`running on ${PORT}`)
+})
